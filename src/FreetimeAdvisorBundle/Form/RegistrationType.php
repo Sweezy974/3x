cf2863290adce78875673a0c50475d6c37401b0e
sweezy974 <sweezy974@live.fr>
2018-03-06 22:42:55 +0400

    multiple choices for user category

--- src/FreetimeAdvisorBundle/Form/RegistrationType.php
+++ src/FreetimeAdvisorBundle/Form/RegistrationType.php
@@ -6,9 +6,11 @@ use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Vich\UploaderBundle\Form\Type\VichImageType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\BirthdayType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use FreetimeAdvisorBundle\Entity\Category;
 
 class RegistrationType extends AbstractType
 
@@ -24,6 +26,21 @@ class RegistrationType extends AbstractType
       'Yes' => NULL,
       'No' => false,)))
     ->add('imageFile', VichImageType::class, array('required' => true,'label'=>false,'attr' => array('class' => 'col s12 m10 l10 offset-m1 offset-l1 center')))
+    ->add('favorite_categories', EntityType::class, array(
+      'mapped'=>false,
+        // looks for choices from this entity
+        'class' => 'FreetimeAdvisorBundle:Category',
+        'choice_value' => function (Category $category = null) {
+            return $category ? $category->getId() : '';
+        },
+        // uses the User.username property as the visible option string
+        'choice_label' => 'name',
+
+        // used to render a select box, check boxes or radios
+        'multiple' => true,
+
+        // 'expanded' => true,
+    ))
     ->add('save', SubmitType::class, array('label' => 'envoyer','attr' => array('class' => '')))
     ;
   }
