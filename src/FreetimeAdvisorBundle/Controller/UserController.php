3d4688da4fbdc2d1d9d871ea6a0f574fe8fa2364
fredBoyer <sweezy974@live.fr>
2018-03-22 11:23:41 +0400

    user hobbies

--- src/FreetimeAdvisorBundle/Controller/UserController.php
+++ src/FreetimeAdvisorBundle/Controller/UserController.php
@@ -4,6 +4,9 @@ namespace FreetimeAdvisorBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Symfony\Component\HttpFoundation\Request;
+use FreetimeAdvisorBundle\Entity\Hobbies;
 
 class UserController extends Controller
 {
@@ -20,6 +23,53 @@ class UserController extends Controller
     return $this->render('@FreetimeAdvisorBundle/Resources/views/user/dashboard/index.html.twig', array(
       'places' => $places,
       'advices' => $advices
+    ));
+  }
+
+  /**
+  * @Route("/user/hobbies/select", name="new_place")
+  * @Method({"GET","POST"})
+  */
+  public function selectHobbies(Request $request)
+  {
+    $user = $this->getUser();
+    $user->getId();
+    $hobbies = new Hobbies();
+    $form = $this->createForm('FreetimeAdvisorBundle\Form\HobbiesType', $hobbies);
+    $form->handleRequest($request);
+    $hobbies->setUser($user);
+    if ($form->isSubmitted() && $form->isValid()) {
+      $em = $this->getDoctrine()->getManager();
+      $em->persist($hobbies);
+      $em->flush();
+
+      return $this->redirectToRoute('dashboard');
+    }
+    return $this->render('@FreetimeAdvisorBundle/Resources/views/user/hobbies/new.html.twig', array(
+      'form' => $form->createView(),
+    ));
+  }
+
+  /**
+  * @Route("user/{pseudo}/hobbies/{id}/edit", name="edit_hobbies")
+  * @Method({"GET","POST"})
+  */
+  public function editHobbies(Hobbies $hobbies ,Request $request)
+  {
+    $user = $this->getUser();
+    $user->getId();
+    $editForm = $this->createForm('FreetimeAdvisorBundle\Form\HobbiesType', $hobbies);
+    $editForm->handleRequest($request);
+    if ($editForm->isSubmitted() && $editForm->isValid()) {
+      $em = $this->getDoctrine()->getManager();
+      $em->persist($hobbies);
+      $em->flush();
+
+      return $this->redirectToRoute('dashboard');
+    }
+    return $this->render('@FreetimeAdvisorBundle/Resources/views/user/hobbies/edit.html.twig', array(
+      'hobbies' => $hobbies,
+      'edit_form' => $editForm->createView(),
     ));
   }
 }
---
+++ src/FreetimeAdvisorBundle/Form/HobbiesType.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace FreetimeAdvisorBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use FreetimeAdvisorBundle\Entity\Category;
+
+class HobbiesType extends AbstractType
+{
+    /**
+    * {@inheritdoc}
+    */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+        ->add('first', EntityType::class, array(
+            // looks for choices from this entity
+            'class' => 'FreetimeAdvisorBundle:Category',
+            'mapped'=>true,
+            'choice_label' => 'name',
+            // 'multiple' => 'true',
+        ))
+        ->add('second', EntityType::class, array(
+            // looks for choices from this entity
+            'class' => 'FreetimeAdvisorBundle:Category',
+            'mapped'=>true,
+            'choice_label' => 'name',
+            // 'multiple' => 'true',
+        ))
+        ->add('third', EntityType::class, array(
+            // looks for choices from this entity
+            'class' => 'FreetimeAdvisorBundle:Category',
+            'mapped'=>true,
+            'choice_label' => 'name',
+            // 'multiple' => 'true',
+        ))
+        ->add('save', SubmitType::class, array('label' => 'envoyer','attr' => array('class' => '')));
+    }/**
+    * {@inheritdoc}
+    */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'FreetimeAdvisorBundle\Entity\Hobbies'
+        ));
+    }
+
+    /**
+    * {@inheritdoc}
+    */
+    public function getBlockPrefix()
+    {
+        return 'freetimeadvisorbundle_hobbies';
+    }
+
+
+}
