464a2cd1724d1df6fb4e0cbc3b723aec170fce07
fredBoyer <sweezy974@live.fr>
2018-04-04 15:06:01 +0400

    fix parent constraint of deleteAdvice with photo

--- src/FreetimeAdvisorBundle/Controller/AdviceController.php
+++ src/FreetimeAdvisorBundle/Controller/AdviceController.php
@@ -5,6 +5,7 @@ namespace FreetimeAdvisorBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use FreetimeAdvisorBundle\Entity\Advice;
+use FreetimeAdvisorBundle\Entity\Place;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Component\HttpFoundation\Request;
 
@@ -39,11 +40,21 @@ class AdviceController extends Controller
   * @Route("place/{name}/advice/{id}/delete", name="delete_advice")
   * @Method({"GET", "DELETE"})
   */
-  public function deleteAdvice(Advice $advice)
+  public function deleteAdvice(Advice $advice,Request $request)
   {
+    $adviceId = $advice->getId();
+    $placeId = $advice->getPlace();
     $em = $this->getDoctrine()->getManager();
+    $photos = $em->getRepository('FreetimeAdvisorBundle:Photo')->findBy(array('advice'=>$adviceId,'place' => $placeId));
+    // si pas de photo pour cet avis
+    if (!empty($photos)) {
+      foreach ($photos as $photo) {
+        $em->remove($photo);
+      }
+    }
     $em->remove($advice);
     $em->flush();
     return $this->redirectToRoute('user_dashboard');
+
   }
 }
